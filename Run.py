0e5a7fe79ed8157d475f67b24dc91c99f42ec149
zhengjiawen <zhengjiawen77@gmail.com>
2019-03-26 19:47:04 +0800

    uodate resize

--- Run.py
+++ Run.py
@@ -10,8 +10,20 @@ from humanseg import Humanseg as H
 from PIL import Image,ImageTk
 
 
+def resizeImg(img):
+    targetSize = 600
+    maxSize = 1100
 
+    h = img.shape[0]
+    w = img.shape[1]
+    minDim = min(w, h)
+    maxDim = max(w, h)
 
+    scale = targetSize / minDim
+    if scale * maxDim > maxSize:
+        scale = maxSize / maxDim
+
+    return scale
 
 
 class segWindows(QWidget, Ui_Dialog):
@@ -20,44 +32,24 @@ class segWindows(QWidget, Ui_Dialog):
         self.setupUi(self)
         self.imgName = 'img/xixi.jpg'
 
-    def resizeImg(img):
-        targetSize = 600
-        maxSize = 800
-        h = img.shape[0]
-        w = img.shape[1]
-        minDim = min(w, h)
-        maxDim = max(w, h)
 
-        scale = targetSize / minDim
-        if scale * maxDim > maxSize:
-            scale = maxSize / maxDim
-
-        return scale
 
 
     def openImage(self):
         self.imgName = QFileDialog.getOpenFileName(self, 'Open Image',"", "*.jpg;;*.png;;All Files(*)" )[0]
         if len(self.imgName):
             img = cv.imread(self.imgName)
-            # scale = resizeImg(img)
-            # img = cv.resize(img, (round(img.shape[1]*scale),round(img.shape[0]*scale)))
+
             img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-            # print(img.shape)
-            # cv.imwrite('resizeImg.jpg', img)
-            # img = img.copy()
+
             x = img.shape[1]
             y = img.shape[0]
             frame = QImage(img, x,y,QImage.Format_RGB888)
-            # img = cv.imread(self.imgName, -1)
-            # # img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-            # x = img.shape[1]
-            # y = img.shape[0]
-            # frame = QImage(img, x,y,QImage.Format_ARGB32)
+
 
             pix = QPixmap.fromImage(frame)
             self.item = QGraphicsPixmapItem(pix)  # 创建像素图元
-            # self.item.setScale(self.zoomscale)
             self.scene = QGraphicsScene()  # 创建场景
             self.scene.addItem(self.item)
             self.originImg.setScene(self.scene)
@@ -73,7 +65,6 @@ class segWindows(QWidget, Ui_Dialog):
         print('output path:'+str(output_path))
 
         img = cv.imread(output_path, -1)
-        # img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         x = img.shape[1]
         y = img.shape[0]
         frame2 = QImage(img, x, y, QImage.Format_ARGB32)
